d75479df9ebf9fbb8765f041f56118eb5274ffb5
aathik <aathiktr@gmail.com>
2023-11-01 23:14:46 -0400

    SecondCommit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function addCategoryRow() {
     var cell2 = newRow.insertCell(1);
     var cell3 = newRow.insertCell(2);
     var cell4 = newRow.insertCell(3);
+    var cell5 = newRow.insertCell(4);
 
     cell1.innerHTML = '<input type="text" name="category_name[]" required>';
     cell2.innerHTML = `
@@ -14,12 +15,13 @@ function addCategoryRow() {
             <option value="number">Number</option>
             <option value="descriptive">Descriptive</option>
         </select>`;
-    cell3.innerHTML = `
+    cell3.innerHTML = `<input type="number" step="0.01" name="weight[]" placeholder="Remaining: 1.00" oninput="updateRemainingWeight(this)"><span class="error-message" style="font-size: 10px;"></span>`;    
+    cell4.innerHTML = `
         <select name="direction_to_optimize[]">
             <option value="up">Up</option>
             <option value="down">Down</option>
         </select>`;
-    cell4.innerHTML = `
+    cell5.innerHTML = `
         <span class="delete-icon" onclick="deleteRow(this)">🗑️</span>
         <span class="clear-icon" onclick="clearRow(this)">🔄</span>`;
 }
@@ -52,6 +54,42 @@ function clearRow(element) {
     }
 }
 
+///ADDD VALIDATION PLS
+function updateRemainingWeight(input) {
+    validateWeight(input);
+    var table = document.querySelector("table");
+    var weightInputs = table.querySelectorAll('input[name="weight[]"]');
+    
+    var totalWeight = 1.0;
+    
+    weightInputs.forEach(function(weightInput) {
+        var weightValue = parseFloat(weightInput.value) || 0;
+        totalWeight -= weightValue;
+    });
+    
+    var remainingWeight = totalWeight.toFixed(2);
+    
+    weightInputs.forEach(function(weightInput) {
+        weightInput.placeholder = "Remaining: " + remainingWeight;
+    });
+}
+
+function validateWeight(input) {
+    var value = parseFloat(input.value);
+    console.log(value);
+    var errorMessage = input.parentElement.querySelector('.error-message');
+    
+    if (isNaN(value) || value < 0 || value > 1) {
+        errorMessage.textContent = "Weight must be a number between 0 and 1.";
+        return false;
+    } else {
+        errorMessage.textContent = "";
+    }
+    return true;
+}
+
+
+//
 function getCategoryData() {
     var table = document.querySelector("table");
     var categoryData = [];
@@ -62,11 +100,13 @@ function getCategoryData() {
         
         var categoryName = cells[0].querySelector('input[name="category_name[]"]').value;
         var scoringType = cells[1].querySelector('select[name="scoring_type[]"]').value;
-        var directionToOptimize = cells[2].querySelector('select[name="direction_to_optimize[]"]').value;
+        var weight = cells[2].querySelector('input[name="weight[]"]').value;
+        var directionToOptimize = cells[3].querySelector('select[name="direction_to_optimize[]"]').value;
         
         categoryData.push({
             categoryName: categoryName,
             scoringType: scoringType,
+            weight: weight,
             directionToOptimize: directionToOptimize
         });
     }
@@ -81,7 +121,7 @@ function getScenarioData() {
     for (var i = 1; i < table.rows.length; i++) {
         var row = table.rows[i];
         var cells = row.cells;
-        
+        // console.log(cells);
         var scenarioName = cells[0].querySelector('input[name="scenario_name[]"]').value;
         // var scenarioValue = cells[1].querySelector('input[name="scenario_value[]"]').value;
         
@@ -93,6 +133,36 @@ function getScenarioData() {
     
     return scenarioData;
 }
+
+function getValuesTableData(){
+    var table = document.getElementById("table-container").querySelector("table");
+    var values = [];
+    var rows = table.rows;
+
+    // Iterate through rows, starting from the first data row (skip the header)
+    for (var i = 1; i < rows.length; i++) {
+        var scenario = { data: {} };
+        var cells = rows[i].cells;
+
+        for (var j = 1; j < cells.length; j++) {
+            var inputElement = cells[j].querySelector('input, select');
+            var columnName = table.rows[0].cells[j].textContent;
+
+            if (inputElement) {
+                // Retrieve the value from the input element
+                var value = inputElement.value;
+                scenario.data[columnName] = value;
+            }
+        }
+
+        // Add the scenario data to the values array
+        values.push(scenario);
+    }
+    console.log(values);
+    return values;
+}
+
+
 
 function valueTable(){
 
@@ -168,24 +238,25 @@ function valueTable(){
     }
     
     tableContainer.appendChild(table);
+    var submit_button = document.getElementById("temp-submit-button");
+    submit_button.style.display = "block";
+
+}
+
+function calculateScore(){
+    var categoryData = getCategoryData(); 
+    var scenarioData = getScenarioData();
+    var valuesData = getValuesTableData();
+    
 
 }
 
 
+
+
+
 // // Get the modal
  var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-//   console.log("button pressed");
-// }
 
 function modalWindowOpen() {
     var modal = document.getElementById("myModal");
@@ -198,10 +269,7 @@ function closeWindowOpen() {
     modal.style.display = "none";
 }
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-  
-// }
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.addEventListener("click", closeWindowWhenClickedAnywhere);
